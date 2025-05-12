2854990c2caac5d15cd2f9f5d81bee7d8b13913a
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-05-12 22:51:53 +0630

    Worker Queues

--- HelloWorld/receive.go
+++ HelloWorld/receive.go
@@ -1,59 +1,59 @@
-package main
+// package main
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	amqp "github.com/rabbitmq/amqp091-go"
-)
+// 	amqp "github.com/rabbitmq/amqp091-go"
+// )
 
-func failOnError(err error, msg string) {		
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
-}
+// func failOnError(err error, msg string) {		
+// 	if err != nil {
+// 		log.Panicf("%s: %s", msg, err)
+// 	}
+// }
 
 
 
-func main() {		
+// func main() {		
 
-		con, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-		failOnError(err, "Failed to connect to RabbitMQ")
-		defer con.Close()
+// 		con, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+// 		failOnError(err, "Failed to connect to RabbitMQ")
+// 		defer con.Close()
 
-		ch, err := con.Channel()
-		failOnError(err, "Failed to open a channel")
-		defer ch.Close()
+// 		ch, err := con.Channel()
+// 		failOnError(err, "Failed to open a channel")
+// 		defer ch.Close()
 
-		q, err := ch.QueueDeclare(
-			"hello", // name
-			false,   // durable		
-			false,   // delete when unused
-			false,   // exclusive
-			false,   // no-wait	
-			nil,     // arguments
-		)
+// 		q, err := ch.QueueDeclare(
+// 			"hello", // name
+// 			false,   // durable		
+// 			false,   // delete when unused
+// 			false,   // exclusive
+// 			false,   // no-wait	
+// 			nil,     // arguments
+// 		)
 
-		failOnError(err, "Failed to declare a queue")	
-		msgs, err := ch.Consume(	
-			q.Name, // queue
-			"",     // consumer
-			true,   // auto-ack
-			false, // exclusive		
-			false, // no-local
-			false, // no-wait	
-			nil,   // args
-		)
-		failOnError(err, "Failed to register a consumer")
-		forever := make(chan struct{})
+// 		failOnError(err, "Failed to declare a queue")	
+// 		msgs, err := ch.Consume(	
+// 			q.Name, // queue
+// 			"",     // consumer
+// 			true,   // auto-ack
+// 			false, // exclusive		
+// 			false, // no-local
+// 			false, // no-wait	
+// 			nil,   // args
+// 		)
+// 		failOnError(err, "Failed to register a consumer")
+// 		forever := make(chan struct{})
 
-		go func() {
-			for d := range msgs {
-				log.Printf("Received a message: %s", d.Body)
-			}
-		}()
+// 		go func() {
+// 			for d := range msgs {
+// 				log.Printf("Received a message: %s", d.Body)
+// 			}
+// 		}()
 	
-		log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-		<-forever
+// 		log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+// 		<-forever
 
-}
+// }
 
